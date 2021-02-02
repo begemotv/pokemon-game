9b74946f64a52ed733a36fa830365309ce8ee33c
begemotv <62621854+begemotv@users.noreply.github.com>
2021-02-02 13:36:24 +0200

    homework 4

---
+++ src/routes/home/index.js
@@ -0,0 +1,64 @@
+import background from '../../img/bg1.jpg'
+
+import Footer from '../../components/footer';
+import Header from '../../components/header';
+import Layout from '../../components/layout';
+import PokemonCard from '../../components/pokemonCard';
+import MenuHeader from '../../components/menuHeader';
+import logoImg from '../../img/logo.png';
+import {POKEMONS} from '../../mocks/pokemons';
+
+const HomePage = ({onPageChange}) => {
+    const handleButtonClick = (page) => {
+        onPageChange && onPageChange(page)
+    }
+
+    return (
+        <>
+        <MenuHeader />
+        <Header
+            title='Pokemon Game'
+            descr='This is simple triple triad card game'
+            onButtonClick={handleButtonClick}
+        />
+        <Layout
+            id='rules'
+            title='Rules'
+            urlBg={background}
+        >
+            <p>In the game two players face off against one another, one side playing as "blue", the other as "red" on a 3x3 grid.</p>
+        </Layout>
+        <Layout
+            id='cards'
+            title='Cards'
+            colorBg='#202736'
+        >
+            <img src={logoImg} alt='logo' />
+            <p style={{color: 'white'}}>Each player has five cards in a hand and the aim is to capture the opponent's cards by turning them into the player's own color of red or blue.</p>
+            <div className='flex'>
+            {
+                POKEMONS.map((item) => 
+                <PokemonCard 
+                    key={item.id}
+                    id={item.id}
+                    img={item.img}
+                    name={item.name}
+                    type={item.type}
+                    values={item.values} 
+                />)
+            }
+            </div>
+        </Layout>
+        <Layout
+            id='about'
+            title='Full rules'
+            urlBg={background}
+        >
+            <p>To win, a majority of the total ten cards played (including the one card that is not placed on the board) must be of the player's card color. To do this, the player must capture cards by placing a card adjacent to an opponent's card whereupon the 'ranks' of the sides where the two cards touch will be compared. If the rank of the opponent's card is higher than the player's card, the player's card will be captured and turned into the opponent's color. If the player's rank is higher, the opponent's card will be captured and changed into the player's color instead. </p>
+        </Layout>
+        <Footer />
+        </>
+    );
+};
+
+export default HomePage;
